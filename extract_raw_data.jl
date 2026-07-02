1c1cc280f30dbbc7cd42d0fe42e24595f6d5d7b0
Alex <alexcoumes8@gmail.com>
2026-07-02 10:10:12 +0200

    Update extract_raw_data.jl pour toutes les databases

--- extract_raw_data.jl
+++ extract_raw_data.jl
@@ -4,32 +4,41 @@ using Eegle
 
 # PUT HERE path to the MI folder of the FII corpus on your PC
 # MIDir = joinpath(@__DIR__, "MI") 
-MIDir = "C:/Users/coumesa/Documents/BCI/data/FII_BCI_Corpus/MI/Zhou2016/subject_01_session_01.npz"
+MIDir = "C:/Users/coumesa/Documents/BCI/data/FII_BCI_Corpus/MI"
 classes = ["left_hand", "right_hand"]; # or for example ["left_hand", "right_hand"]; ["feet",]   # Chosen classes
 
 # select MI databases comprising the given 'classes' and minimum number of trials
 inclusion = (("tpc", x -> minimum(values(x)) > 24),)
 
-# DBs = selectDB(MIDir, :MI; classes, inclusion); # Select databases
+outDir = "C:/Users/coumesa/Documents/BCI/data/processed_data_raw"    # output to save the tensor 
 
-outDir = "C:/Users/coumesa/Documents/BCI/data/FII_BCI_Corpus/Tensor"    # output to save the tensor 
+DBs = selectDB(MIDir, :MI; classes, inclusion); # Select all the databases folowing the criteria
 
-o = readNY(MIDir; bandPass = (4, 36), upperLimit = 1.2, classes) # read session
+# Create and save all data
+for DB ∈ DBs
+    
+    @info "\n Writin data for databse $(DB.dbName)"
 
-k = length(o.trials)# number of trials
-n = o.ne# number of channels  
-t = o.wl # number of samples  
+    for (d, file) in enumerate(DB.files)
+        
+        println("writing file $d of $(length(DB.files))")
 
-out = Array{Float32, 3}(undef, k, n, t) 
+        o = readNY(file; bandPass = (4, 36), upperLimit = 1.2, classes) # verify for artefact and bandPass
+        
+        k = length(o.trials)# number of trials
+        n = o.ne# number of channels  
+        t = o.wl # number of samples
+        
+        out = Array{Float32, 3}(undef, k, n, t)
 
-@simd for i in 1:k
-    @inbounds out[i, :, :] = Float32.(transpose(o.trials[i]))
+        @simd for i in 1:k
+            @inbounds out[i, :, :] = Float32.(transpose(o.trials[i]))
+        end
+
+        npzwrite(joinpath(outDir,"$(DB.dbName)_$(d)_signal_eeg.npy"), out)
+        npzwrite(joinpath(outDir,"$(DB.dbName)_$(d)_labels_eeg.npy"), o.y)
+    end
+    
 end
-
-labels = o.y
-
-println("Taille du tenseur : ", size(out))
-println("Taille des labels : ", size(labels))
-println("Labels : ", labels)
 
 @info "Finished!"
